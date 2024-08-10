be0d052dbd303272cd7e4fe0ffa366d408fd8fc6
JHAMILCALI <jhamilquea@gmail.com>
2024-08-10 11:27:37 -0400

    orimer comit con el el nadvard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/navegacion/navbar';
+import Index from './Components/paginas';
+import Pagina1 from './Components/paginas/pagina1';
+import Pagina2 from './Components/paginas/pagina2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Index />} />
+          <Route path='/pagina1' element={<Pagina1 />} />
+          <Route path='/pagina2' element={<Pagina2 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Components/paginas/pagina1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const pagina1 = () => {
+  return (
+    <div>
+        <h1>PAGINA DE 1</h1>
+    </div>
+  )
+}
+
+export default pagina1
