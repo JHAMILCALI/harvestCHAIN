8c56cefba59805fd114acbe9cf61dd76d091dda5
JHAMILCALI <jhamilquea@gmail.com>
2024-08-10 19:37:39 -0400

    conectar wallets

---
+++ src/Components/navegacion/ConnectionButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import{useState} from "react"
+export default function ConnectionButton() {
+  const[buttonText, setButtonText] = useState('Conectar wallet')
+  const [account, setAccount] = useState(null)
+  const connectWallet = () => {
+    if(window.ethereum && window.ethereum.isMetaMask){
+        window.ethereum.request({method: 'eth_requestAccounts',})
+        .then(result =>{
+          setAccount(result[0])
+        })
+        .catch(error =>{
+          setButtonText(error.message)
+        })
+    }else{
+        setButtonText('Necesistas tener metamask')
+    }
+  }
+  return (
+    <button onClick={connectWallet}>{buttonText}{account}</button>
+  );
+}
--- src/Components/navegacion/navbar.js
+++ src/Components/navegacion/navbar.js
@@ -1,7 +1,8 @@
+// src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import img1 from '../../assets/img/img1.png';
-
+import ConnectionButton from './ConnectionButton'; // Asegúrate de que la ruta sea correcta
 
 const Navbar = () => {
   return (
@@ -9,7 +10,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <Link to='/'>
-            <img src={img1} width='50%'/>
+            <img src={img1} width='50%' alt="Logo"/>
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -23,9 +24,12 @@ const Navbar = () => {
                 <Link className="nav-link" to='/productor'>Productor</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to='distribuidor'>Distribuidor</Link>
+                <Link className="nav-link" to='/distribuidor'>Distribuidor</Link>
               </li>
             </ul>
+            <div className="navbar-nav">
+              <ConnectionButton/>
+            </div>
           </div>
         </div>
       </nav>
