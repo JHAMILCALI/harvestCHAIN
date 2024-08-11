1dc73479334acaaf397f5f9606cfa5febc8db770
JHAMILCALI <jhamilquea@gmail.com>
2024-08-11 04:33:00 -0400

    PRUEBA

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/navegacion/navbar';
-import Index from './Components/paginas/index';
+import Index from './Components/paginas/';
 import Productor from './Components/paginas/productor';
 import Distribuidor from './Components/paginas/distribuidor';
 
